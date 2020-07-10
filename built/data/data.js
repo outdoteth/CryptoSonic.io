03be6ccd10d3a986686570a51983671e8775210d
Dylan-Kerler <dylanphoon@gmail.com>
2020-07-10 21:43:37 +0100

    Update

--- built/data/data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.start = void 0;
-var SimpleStats = require("./stats/simple");
-function start() {
-    SimpleStats.start();
-}
-exports.start = start;
